d40a020168ba52c0f57f6cc4e821391ab161788f
Drpto <dr.juniornoob@gmail.com>
2025-02-11 09:46:59 +0530

    violet: Add Quicktap, Faceunlock, Blur support
    Signed-off-by: Drpto <dr.juniornoob@gmail.com>

--- lineage_violet.mk
+++ lineage_violet.mk
@@ -14,6 +14,12 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 # Inherit MiuiCamera
 $(call inherit-product, vendor/MiuiCamera/config.mk)
 
+#Device Props
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_ENABLE_BLUR := false
+TARGET_DEBLOAT := true
+
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_violet
 PRODUCT_DEVICE := violet
