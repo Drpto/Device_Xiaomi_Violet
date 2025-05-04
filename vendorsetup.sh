98125ce54f7417fa78074823c159968a7196536a
drpto <dr.juniornoob@gmail.com>
2025-05-04 21:56:16 +0530

    violet: Track nessery repos
    Signed-off-by: drpto <dr.juniornoob@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,12 @@
+# vendor & Kernel stuff
+git clone https://github.com/Drpto/vendor_xiaomi_violet.git -b 15 vendor/xiaomi/violet 
+git clone https://github.com/Joker-V2/kernel_xiaomi_violet.git -b fifteen kernel/xiaomi/violet
+
+# Hardware stuff
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-22.2 hardware/xiaomi
+
+#Firmware
+git clone https://gitlab.com/Drpto/vendor_xiaomi-firmware_violet.git -b fifteen vendor/xiaomi-firmware/violet
+
+# Miui Camera stuff
+git clone https://gitlab.com/Joker-V2/android_vendor_MiuiCamera.git -b fifteen vendor/MiuiCamera
